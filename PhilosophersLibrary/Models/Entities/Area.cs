a0f896fbe4faceb0c5c1bdf648444a8fb3990b15
Stephen Shine <stephen.shine88@gmail.com>
2016-10-26 20:53:56 +0100

    Adding Area model

--- PhilosophersLibrary/Models/Entities/Area.cs
+++ PhilosophersLibrary/Models/Entities/Area.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,11 @@ namespace PhilosophersLibrary.Models.Entities
 {
     public class Area
     {
+        public int AreaID { get; set; }
+        [Display(Name = "Area")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Philosopher> Philosophers { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
     }
 }
