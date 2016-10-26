641bbeb18a0f0f354780ffd341a4035a7a7fed70
Stephen Shine <stephen.shine88@gmail.com>
2016-10-26 20:48:52 +0100

    Adding Philosopher model.

---
+++ PhilosophersLibrary/Models/Entities/Philosopher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhilosophersLibrary.Models.Entities
+{
+    public class Philosopher
+    {
+        public int PhilosopherID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Display(Name = "Date of birth")]
+        public DateTime DateOfBirth { get; set; }
+        [Display(Name = "Date of death")]
+        public DateTime DateOfDeath { get; set; }
+        public Boolean IsAlive { get; set; }
+        public int NationalityID { get; set; }
+        public int AreaID { get; set; }
+        public string Description { get; set; }
+
+        // Navigation properties - defined as virtual to use LazyLoading
+        public virtual Nationality Nationality { get; set; }
+        public virtual Area Area { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
