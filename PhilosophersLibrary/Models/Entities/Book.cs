d2f9e94b81beb62c4e67317a4f6a58e65f928fe0
Stephen Shine <stephen.shine88@gmail.com>
2016-10-26 20:55:49 +0100

    Adding Book model

--- PhilosophersLibrary/Models/Entities/Book.cs
+++ PhilosophersLibrary/Models/Entities/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,14 @@ namespace PhilosophersLibrary.Models.Entities
 {
     public class Book
     {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Philosopher")]
+        public int PhilosopherID { get; set; }
+        [Display(Name = "Area")]
+        public int AreaID { get; set; }
+
+        public virtual Philosopher Philosopher { get; set; }
+        public virtual Area Area { get; set; }
     }
 }
