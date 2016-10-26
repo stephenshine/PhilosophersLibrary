32a654cf3052014ea5be3cc7b5fcaedb18aa65d1
Stephen Shine <stephen.shine88@gmail.com>
2016-10-26 20:52:12 +0100

    Adding Nationality model, and empty book and area models

---
+++ PhilosophersLibrary/Models/Entities/Nationality.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhilosophersLibrary.Models.Entities
+{
+    public class Nationality
+    {
+        public int NationalityID { get; set; }
+        [Display(Name = "Nationality")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Philosopher> Philosophers { get; set; }
+    }
+}
