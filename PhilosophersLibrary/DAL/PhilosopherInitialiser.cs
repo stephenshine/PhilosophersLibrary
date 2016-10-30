3940a6c5ae2735436ab6f5b9de595dc991a5ee72
Stephen Shine <stephen.shine88@gmail.com>
2016-10-30 21:20:49 +0000

    Commenting out philosophers and books from PInitialiser - Areas and Nationalities run successfully.

--- PhilosophersLibrary/DAL/PhilosopherInitialiser.cs
+++ PhilosophersLibrary/DAL/PhilosopherInitialiser.cs
@@ -10,40 +10,40 @@ namespace PhilosophersLibrary.DAL
     {
         protected override void Seed(PhilosopherContext context)
         {
-            var philosophers = new List<Philosopher>{
-                new Philosopher {
-                    FirstName = "Bertrand",
-                    LastName = "Russell",
-                    DateOfBirth = DateTime.Parse("1872-05-18"),
-                    DateOfDeath = DateTime.Parse("1970-02-02"),
-                    IsAlive = false,
-                    NationalityID = 1,
-                    AreaID = 7,
-                    Description = "Here's some text about Bertrand Russell"
-                },
-                new Philosopher {
-                    FirstName = "Immanuel",
-                    LastName = "Kant",
-                    DateOfBirth = DateTime.Parse("1724-04-22"),
-                    DateOfDeath = DateTime.Parse("1804-02-12"),
-                    IsAlive = false,
-                    NationalityID = 3,
-                    AreaID = 1,
-                    Description = "Here's some text about Immanuel Kant"
-                },
-                new Philosopher {
-                    FirstName = "John",
-                    LastName = "Rawls",
-                    DateOfBirth = DateTime.Parse("1921-02-21"),
-                    DateOfDeath = DateTime.Parse("2002-11-24"),
-                    IsAlive = false,
-                    NationalityID = 9,
-                    AreaID = 3,
-                    Description = "Here's some text about John Rawls"
-                }
-            };
-            philosophers.ForEach(p => context.Philosopher.Add(p));
-            context.SaveChanges();
+            //var philosophers = new List<Philosopher>{
+            //    new Philosopher {
+            //        FirstName = "Bertrand",
+            //        LastName = "Russell",
+            //        DateOfBirth = DateTime.Parse("1872-05-18"),
+            //        DateOfDeath = DateTime.Parse("1970-02-02"),
+            //        IsAlive = false,
+            //        NationalityID = 1,
+            //        AreaID = 7,
+            //        Description = "Here's some text about Bertrand Russell"
+            //    },
+            //    new Philosopher {
+            //        FirstName = "Immanuel",
+            //        LastName = "Kant",
+            //        DateOfBirth = DateTime.Parse("1724-04-22"),
+            //        DateOfDeath = DateTime.Parse("1804-02-12"),
+            //        IsAlive = false,
+            //        NationalityID = 3,
+            //        AreaID = 1,
+            //        Description = "Here's some text about Immanuel Kant"
+            //    },
+            //    new Philosopher {
+            //        FirstName = "John",
+            //        LastName = "Rawls",
+            //        DateOfBirth = DateTime.Parse("1921-02-21"),
+            //        DateOfDeath = DateTime.Parse("2002-11-24"),
+            //        IsAlive = false,
+            //        NationalityID = 9,
+            //        AreaID = 3,
+            //        Description = "Here's some text about John Rawls"
+            //    }
+            //};
+            //philosophers.ForEach(p => context.Philosopher.Add(p));
+            //context.SaveChanges();
 
             var nationalities = new List<Nationality>
             {
@@ -74,41 +74,41 @@ namespace PhilosophersLibrary.DAL
             areas.ForEach(a => context.Area.Add(a));
             context.SaveChanges();
 
-            var books = new List<Book>
-            {
-                new Book {
-                    Title = "The impact of science on society",
-                    PhilosopherID = 1,
-                    AreaID = 6
-                },
-                new Book {
-                    Title = "The analysis of mind",
-                    PhilosopherID = 1,
-                    AreaID = 4
-                },
-                new Book {
-                    Title = "Marriage and morals",
-                    PhilosopherID = 1,
-                    AreaID = 8
-                },
-                new Book{
-                    Title = "Critique of pure reason",
-                    PhilosopherID = 2,
-                    AreaID = 9
-                },
-                new Book{
-                    Title = "The metaphysics of morals",
-                    PhilosopherID = 2,
-                    AreaID = 8
-                },
-                new Book{
-                    Title = "A theory of justice",
-                    PhilosopherID = 3,
-                    AreaID = 3
-                }
-            };
-            books.ForEach(b => context.Book.Add(b));
-            context.SaveChanges();
+            //var books = new List<Book>
+            //{
+            //    new Book {
+            //        Title = "The impact of science on society",
+            //        PhilosopherID = 1,
+            //        AreaID = 6
+            //    },
+            //    new Book {
+            //        Title = "The analysis of mind",
+            //        PhilosopherID = 1,
+            //        AreaID = 4
+            //    },
+            //    new Book {
+            //        Title = "Marriage and morals",
+            //        PhilosopherID = 1,
+            //        AreaID = 8
+            //    },
+            //    new Book{
+            //        Title = "Critique of pure reason",
+            //        PhilosopherID = 2,
+            //        AreaID = 9
+            //    },
+            //    new Book{
+            //        Title = "The metaphysics of morals",
+            //        PhilosopherID = 2,
+            //        AreaID = 8
+            //    },
+            //    new Book{
+            //        Title = "A theory of justice",
+            //        PhilosopherID = 3,
+            //        AreaID = 3
+            //    }
+            //};
+            //books.ForEach(b => context.Book.Add(b));
+            //context.SaveChanges();
         }
     }
 }
